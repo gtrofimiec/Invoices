5515fb343751e2d67795bf810fcb9b6b2b730060
Grzegorz Trofimiec <gtrofimiec@gmail.com>
2022-02-24 11:54:54 +0100

    JAR and PostgreSQL fixed

--- src/main/java/com/myprojects/invoice/InvoiceApplication.java
+++ src/main/java/com/myprojects/invoice/InvoiceApplication.java
@@ -6,15 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class InvoiceApplication extends SpringBootServletInitializer {
 public class InvoiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InvoiceApplication.class, args);
     }
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(InvoiceApplication.class);
-//    }
 }
