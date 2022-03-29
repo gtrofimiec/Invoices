ad1d4287a93e23f689dd290e10ce91b8a178a100
Grzegorz Trofimiec <gtrofimiec@gmail.com>
2022-03-29 13:37:19 +0200

    Deploy to Heroku - take 3

--- src/main/java/com/myprojects/invoice/config/AdminConfig.java
+++ src/main/java/com/myprojects/invoice/config/AdminConfig.java
@@ -12,6 +12,4 @@ public class AdminConfig {
     private String adminMail;
     @Value("${admin.name}")
     private String adminName;
-    @Value("${info.company.name}")
-    private String companyName;
 }
